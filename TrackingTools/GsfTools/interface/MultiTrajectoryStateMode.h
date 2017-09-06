f01665af3e4c825aacf4927a0813e02fd63838a0
Wolfgang Adam <wolfgang.adam@cern.ch>
2017-09-06 09:30:26 +0200

    add methods for access to cartesian (1D) multistates

--- TrackingTools/GsfTools/interface/MultiTrajectoryStateMode.h
+++ TrackingTools/GsfTools/interface/MultiTrajectoryStateMode.h
@@ -5,6 +5,9 @@
 
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 #include "DataFormats/GeometryVector/interface/GlobalVector.h"
+#include "TrackingTools/GsfTools/interface/MultiGaussianState1D.h"
+#include <vector>
+using std::vector;
 
 class TrajectoryStateOnSurface;
 
@@ -14,6 +17,7 @@ public:
    *  Return value true for success. */
   bool momentumFromModeCartesian (const TrajectoryStateOnSurface tsos,
 				  GlobalVector& momentum) const;
+  vector<MultiGaussianState1D> momentumMultiState1DCartesian (const TrajectoryStateOnSurface tsos) const;
   /** Cartesian position from 1D mode calculation in cartesian co-ordinates.
    *  Return value true for success. */
   bool positionFromModeCartesian (const TrajectoryStateOnSurface tsos,
@@ -22,6 +26,7 @@ public:
    *  Return value true for success. */
   bool momentumFromModeLocal (const TrajectoryStateOnSurface tsos,
 			      GlobalVector& momentum) const;
+  vector<MultiGaussianState1D> momentumMultiState1DLocal (const TrajectoryStateOnSurface tsos) const;
   /** Cartesian position from 1D mode calculation in local co-ordinates (x, y).
    *  Return value true for success. */
   bool positionFromModeLocal (const TrajectoryStateOnSurface tsos,
@@ -36,6 +41,7 @@ public:
    *  Return value true for success. */
   bool momentumFromModePPhiEta (const TrajectoryStateOnSurface tsos,
 				GlobalVector& momentum) const;
+  vector<MultiGaussianState1D> momentumMultiState1DPPhiEta (const TrajectoryStateOnSurface tsos) const;
   /** Charge from 1D mode calculation in q/p. Q=0 in case of failure. */
   int chargeFromMode (const TrajectoryStateOnSurface tsos) const;
 };
--- TrackingTools/GsfTools/src/MultiTrajectoryStateMode.cc
+++ TrackingTools/GsfTools/src/MultiTrajectoryStateMode.cc
@@ -46,14 +46,54 @@ MultiTrajectoryStateMode::momentumFromModeCartesian (const TrajectoryStateOnSurf
   MultiGaussianState1D pxState(pxStates);
   MultiGaussianState1D pyState(pyStates);
   MultiGaussianState1D pzState(pzStates);
-  GaussianSumUtilities1D pxUtils(pxState);
-  GaussianSumUtilities1D pyUtils(pyState);
-  GaussianSumUtilities1D pzUtils(pzState);
+  GaussianSumUtilities1D pxUtils(pxState); pxUtils.theDebug = true;
+  GaussianSumUtilities1D pyUtils(pyState); pyUtils.theDebug = true;
+  GaussianSumUtilities1D pzUtils(pzState); pzUtils.theDebug = true;
   //
   // cartesian momentum vector from modes
   //
   momentum = GlobalVector(pxUtils.mode().mean(),pyUtils.mode().mean(),pzUtils.mode().mean());
   return true;
+}
+
+vector<MultiGaussianState1D>
+MultiTrajectoryStateMode::momentumMultiState1DCartesian (const TrajectoryStateOnSurface tsos) const
+{
+  //
+  // clear result vector and check validity of the TSOS
+  //
+  vector<MultiGaussianState1D> result;
+  if ( !tsos.isValid() ) {
+    edm::LogInfo("MultiTrajectoryStateMode") << "Cannot calculate mode from invalid TSOS";
+    return result;
+  }
+  //  
+  // 1D mode computation for px, py and pz
+  // 
+  std::vector<TrajectoryStateOnSurface> components(tsos.components());
+  unsigned int numb = components.size();
+  // vectors of components in x, y and z
+  std::vector<SingleGaussianState1D> pxStates; pxStates.reserve(numb);
+  std::vector<SingleGaussianState1D> pyStates; pyStates.reserve(numb);
+  std::vector<SingleGaussianState1D> pzStates; pzStates.reserve(numb);
+  // iteration over components
+  for ( std::vector<TrajectoryStateOnSurface>::const_iterator ic=components.begin();
+	ic!=components.end(); ++ic ) {
+    // extraction of parameters and variances
+    GlobalVector mom(ic->globalMomentum());
+    AlgebraicSymMatrix66 cov(ic->cartesianError().matrix());
+    pxStates.push_back(SingleGaussianState1D(mom.x(),cov(3,3),ic->weight()));
+    pyStates.push_back(SingleGaussianState1D(mom.y(),cov(4,4),ic->weight()));
+    pzStates.push_back(SingleGaussianState1D(mom.z(),cov(5,5),ic->weight()));
+  }
+  //
+  // transformation in 1D multi-states and creation of utility classes
+  //
+  result.push_back(MultiGaussianState1D(pxStates));
+  result.push_back(MultiGaussianState1D(pyStates));
+  result.push_back(MultiGaussianState1D(pzStates));
+
+  return result;
 }
 
 bool
@@ -142,6 +182,34 @@ MultiTrajectoryStateMode::momentumFromModeLocal (const TrajectoryStateOnSurface 
   // conversion to global coordinates
   momentum = tsos.surface().toGlobal(localP);
   return true;
+}
+
+vector<MultiGaussianState1D>
+MultiTrajectoryStateMode::momentumMultiState1DLocal (const TrajectoryStateOnSurface tsos) const
+{
+  //
+  // clear result vector and check validity of the TSOS
+  //
+  vector<MultiGaussianState1D> result;
+  if ( !tsos.isValid() ) {
+    edm::LogInfo("MultiTrajectoryStateMode") << "Cannot calculate mode from invalid TSOS";
+    return result;
+  }
+  //  
+  // mode computation for local co-ordinates q/p, dx/dz, dy/dz
+  //
+  double qpMode(0);
+  double dxdzMode(0);
+  double dydzMode(0);
+  //
+  // first 3 elements of local parameters = q/p, dx/dz, dy/dz
+  //
+  for ( unsigned int iv=0; iv<3; ++iv ) {
+    // extraction of multi-state using helper class
+    MultiGaussianState1D state1D = MultiGaussianStateTransform::multiState1D(tsos,iv);
+    result.push_back(state1D);
+  }
+  return result;
 }
 
 bool
@@ -325,6 +393,70 @@ MultiTrajectoryStateMode::momentumFromModePPhiEta (const TrajectoryStateOnSurfac
   // conversion to a cartesian momentum vector
   momentum = GlobalVector(pt*cos(phi),pt*sin(phi),pz);
   return true;
+}
+
+vector<MultiGaussianState1D>
+MultiTrajectoryStateMode::momentumMultiState1DPPhiEta (const TrajectoryStateOnSurface tsos) const
+{
+  //
+  // clear result vector and check validity of the TSOS
+  //
+  vector<MultiGaussianState1D> result;
+  if ( !tsos.isValid() ) {
+    edm::LogInfo("MultiTrajectoryStateMode") << "Cannot calculate mode from invalid TSOS";
+    return result;
+  }
+  //  
+  // 1D mode computation for p, phi, eta
+  // 
+  std::vector<TrajectoryStateOnSurface> components(tsos.components());
+  unsigned int numb = components.size();
+  // vectors of components in p, phi and eta
+  std::vector<SingleGaussianState1D> pStates; pStates.reserve(numb);
+  std::vector<SingleGaussianState1D> phiStates; phiStates.reserve(numb);
+  std::vector<SingleGaussianState1D> etaStates; etaStates.reserve(numb);
+  // covariances in cartesian and p-phi-eta and jacobian
+  AlgebraicMatrix33 jacobian;
+  AlgebraicSymMatrix33 covCart;
+  AlgebraicSymMatrix33 covPPhiEta;
+  // iteration over components
+  for ( std::vector<TrajectoryStateOnSurface>::const_iterator ic=components.begin();
+	ic!=components.end(); ++ic ) {
+    // parameters
+    GlobalVector mom(ic->globalMomentum());
+    double px = mom.x();
+    double py = mom.y();
+    double pz = mom.z();
+    double p = mom.mag();
+    double pt2 = mom.perp2();
+    double phi = mom.phi();
+    double eta = mom.eta();
+    // jacobian
+    jacobian(0,0) = px/p;
+    jacobian(0,1) = py/p;
+    jacobian(0,2) = pz/p;
+    jacobian(1,0) = py/pt2;
+    jacobian(1,1) = -px/pt2;
+    jacobian(1,2) = 0;
+    jacobian(2,0) = px*pz/(pt2*p);
+    jacobian(2,1) = py*pz/(pt2*p);
+    jacobian(2,2) = -1./p;
+    // extraction of the momentum part from the 6x6 cartesian error matrix
+    // and conversion to p-phi-eta
+    covCart = ic->cartesianError().matrix().Sub<AlgebraicSymMatrix33>(3,3);
+    covPPhiEta = ROOT::Math::Similarity(jacobian,covCart);
+    pStates.push_back(SingleGaussianState1D(p,covPPhiEta(0,0),ic->weight()));
+    phiStates.push_back(SingleGaussianState1D(phi,covPPhiEta(1,1),ic->weight()));
+    etaStates.push_back(SingleGaussianState1D(eta,covPPhiEta(2,2),ic->weight()));
+  }
+  //
+  // transformation in 1D multi-states and creation of utility classes
+  //
+  result.push_back(MultiGaussianState1D(pStates));
+  result.push_back(MultiGaussianState1D(phiStates));
+  result.push_back(MultiGaussianState1D(etaStates));
+
+  return result;
 }
 
 int
